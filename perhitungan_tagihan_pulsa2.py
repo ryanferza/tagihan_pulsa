2bc17dd68a974ba780c65f12af799df9d928b4a1
Wafiqhasan <66243434+Wafiqhasan@users.noreply.github.com>
2020-06-01 19:20:06 +0700

    Update perhitungan_tagihan_pulsa2.py

--- perhitungan_tagihan_pulsa2.py
+++ perhitungan_tagihan_pulsa2.py
@@ -20,3 +20,22 @@ while input_data:
     if answer != "y":
 
         break
+    minggu = input("Untuk minggu ke: ")
+    total_sms = int(input("Jumlah SMS : "))
+    menit_tlp_sama = float(input("Jumlah menit telepon anda ke sesama operator: "))
+    menit_tlp_lain = float(input("Jumlah menit telepon anda ke lain operator: "))
+    total_kuota_sosmed = float(input("Jumlah kuota terpakai untuk sosial media (kb): "))
+    total_kuota_lainnya = float(input("Jumlah kuota terpakai untuk lainnya (kb): "))
+    totalkuota1 = total_kuota_sosmed + total_kuota_lainnya
+    totalkuota2 = 5*totalkuota1
+    totalpulsatlp = 250*menit_tlp_sama + 500*menit_tlp_lain
+    pulsasms = 150*total_sms
+    totalpulsa = pulsasms + totalpulsatlp
+    total_tagihan = totalkuota2 + totalpulsa
+    
+    data.append([minggu,total_sms,menit_tlp_sama,menit_tlp_lain,totalpulsa,total_kuota_sosmed,total_kuota_lainnya,totalkuota2,total_tagihan])
+
+print(data)
+
+print("--------------------------------------------------------------------------------")
+print('Minggu ke-\t Jumlah SMS\t Menit telepon ke sesama operator(menit)\t Menit telepon ke operaator lain(menit)\t Total pulsa (Rp)\t Kuota sosmed (kb)\t Kuota lainnya (kb)\t Tagihan kuota terpakai (Rp)\t Tagihan/minggu')
